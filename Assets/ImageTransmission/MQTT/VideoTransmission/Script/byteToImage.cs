e0900f6c0bf95aa1c13659d462bce8a3a79ebffc
jieguann <60665347+jieguann@users.noreply.github.com>
2021-02-03 18:46:07 -0500

    add three image

--- Assets/ImageTransmission/MQTT/VideoTransmission/Script/byteToImage.cs
+++ Assets/ImageTransmission/MQTT/VideoTransmission/Script/byteToImage.cs
@@ -6,29 +6,64 @@ using M2MqttUnity.Examples;
 
 public class byteToImage : MonoBehaviour
 {
-    public RawImage m_RawImage;
+    public RawImage DepthRawImage, ColorRawImage, HumanRawImage;
     //texture to receive
-    public Texture2D receiveTexture;
+    public Texture2D DepthreceiveTexture, ColorreceiveTexture, HumanreceiveTexture;
     public MQTTTest mqtt;
-    public byte[] receiveBytes;
+    public byte[] DepthReceiveBytes, ColorReceiveBytes, HumanReceiveBytes;
     //public MQTTTest mqtt;
     // Start is called before the first frame update
     void Start()
     {
-        receiveTexture = new Texture2D(2, 2,TextureFormat.RGBA32,false);
+        DepthreceiveTexture  = new Texture2D(2, 2,TextureFormat.RGBA32,false);
+        ColorreceiveTexture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        HumanreceiveTexture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+
+        StartCoroutine(waiter());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator waiter()
     {
-        receiveBytes = mqtt.DepthImageByte;
+        yield return new WaitForSeconds(0.1f);
 
-        if(receiveBytes != null)
+        while (true)
         {
-            receiveTexture.LoadImage(receiveBytes);
-        }
-        
+            yield return new WaitForSeconds(0.1f);
+            DecodeToImage();
 
-        m_RawImage.texture = receiveTexture;
+
+
+        }
+
+
+
+    }
+    // Update is called once per frame
+    void DecodeToImage()
+    {
+        DepthReceiveBytes = mqtt.DepthImageByte;
+        ColorReceiveBytes = mqtt.ColorImageByte;
+        HumanReceiveBytes = mqtt.HumanImageByte;
+
+        if(DepthReceiveBytes != null)
+        {
+            DepthreceiveTexture.LoadImage(DepthReceiveBytes);
+        }
+
+        if (ColorReceiveBytes != null)
+        {
+            ColorreceiveTexture.LoadImage(ColorReceiveBytes);
+        }
+
+        if (HumanReceiveBytes != null)
+        {
+            HumanreceiveTexture.LoadImage(HumanReceiveBytes);
+        }
+
+
+        DepthRawImage.texture = DepthreceiveTexture;
+        ColorRawImage.texture = ColorreceiveTexture;
+        HumanRawImage.texture = HumanreceiveTexture;
     }
 }
